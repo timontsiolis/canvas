f7668e5195df973bab834f41c30cc80d7d02eece
timon <timontsiolis@gmail.com>
2019-02-17 22:16:52 +0100

    Canvas circlesBounce updated.
    -setup via jetbrains-

--- canvasCircleBounce/canvasBounce.js
+++ canvasCircleBounce/canvasBounce.js
@@ -5,25 +5,38 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext("2d");
 
+function Cricle(x, y, rad, dx, dy) {
+    this.x = x;
+    this.y = y;
+    this.rad = rad;
+    this.dx = dx;
+    this.dy = dy;
+    
+    this.draw = function () {
+        c.beginPath();
+        c.arc(x,y,radius,0,Math.PI*2,false);
+        c.strokeStyle = "blue";
+        c.stroke();
+
+        x += dx;
+        y += dy;
+        if (x >= window.innerWidth - radius || x <= radius) dx *= -1;
+        if (y >= window.innerHeight - radius || y <= radius) dy *= -1;
+    }
+}
 var radius = 100*Math.random();
 var x = window.innerWidth/2-radius;
 var y = window.innerHeight/2-radius;
 
 var dx =10*Math.random(), dy =10*Math.random();
 
+var circle = new Cricle(x,y,radius,dx,dy);
+
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0,0,window.innerWidth,window.innerHeight);
+    circle.draw();
 
-    c.beginPath();
-    c.arc(x,y,radius,0,Math.PI*2,false);
-    c.strokeStyle = "blue";
-    c.stroke();
-
-    x += dx;
-    y += dy;
-    if (x >= window.innerWidth - radius || x <= radius) dx *= -1;
-    if (y >= window.innerHeight - radius || y <= radius) dy *= -1;
 }
 
 animate();
